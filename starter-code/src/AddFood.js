2723f902602888aa2b7132731be2b5b2daf18128
watnfoehn <hoehnefelix@googlemail.com>
2018-11-21 12:21:19 +0100

    iteration 1 and 2 are done

---
+++ starter-code/src/AddFood.js
@@ -0,0 +1,15 @@
+
+
+import React, { Component } from 'react'
+
+export default class AddFood extends Component {
+  render() {
+    return (
+      <div>
+        <button>Add Food</button>
+      </div>
+    )
+  }
+}
+
+
--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import jsonFoods from './foods.json'
+import FoodBox from './FoodBox'
+import AddFood from './AddFood'
+
+
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      foods: jsonFoods
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Ironnutrition</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       {(this.state.foods).map((foodthatiamloopingthorugh, i) =>(
+         <div>
+         <FoodBox key={i} food={foodthatiamloopingthorugh} />
+         </div>
+       ))
+      }
+      <AddFood />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
